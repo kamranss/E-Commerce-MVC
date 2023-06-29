acc348af6948f4a27ddf874646ed4f768335199f
kamranss <kamranss@code.edu.az>
2023-06-30 02:45:50 +0400

    Refactor: Account controller updated

---
+++ AllUp2/AllUp2/Services/LoginService/ILoginService.cs
@@ -0,0 +1,10 @@
+﻿using AllUp2.ViewModels.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllUp2.Services.LoginService
+{
+    public interface ILoginService
+    {
+        public void Login(LoginVM loginVM, string? ReturnUrl);
+    }
+}
---
+++ AllUp2/AllUp2/Services/LoginService/LoginService.cs
@@ -0,0 +1,12 @@
+﻿using AllUp2.ViewModels.Account;
+
+namespace AllUp2.Services.LoginService
+{
+    public class LoginService : ILoginService
+    {
+        public void Login(LoginVM loginVM, string? ReturnUrl)
+        {
+           
+        }
+    }
+}
